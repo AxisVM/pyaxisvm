f1cacb262534d0c8bd0671f61b569b9771c88231
Bence Balogh <benceeok@gmail.com>
2021-11-29 19:25:58 +0100

    changes to setup machinery

---
+++ setup.py
@@ -0,0 +1,34 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="axisvm",                     
+    version="0.0.2",                        
+    author="Inter-CAD Ltd",
+    author_email = 'bbalogh@axisvm.eu',
+    url = 'https://github.com/AxisVM/pyaxisvm',   
+    download_url = 'https://github.com/AxisVM/pyaxisvm/archive/refs/tags/v_0_0_2.zip',                     
+    keywords = ['AxisVM', 'Axis', 'Civil Engineering'],
+    description="A python package for AxisVM",
+    long_description=long_description,   
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),   
+    classifiers=[
+        'Development Status :: 3 - Alpha',     
+        'License :: OSI Approved :: MIT License',   
+        'Programming Language :: Python :: 3',
+    ],                                      
+    python_requires='>=3.6',                
+    py_modules=["axisvm"],             
+    package_dir={'':'src'},     
+    install_requires=[            
+          'setuptools',
+          'wheel',
+          'comtypes',
+          'pywin32'
+      ],
+)
+
